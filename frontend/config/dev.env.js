d9ae7ad71d7042695464960816aa09ae398cbbc2
phinguin123 <koysr20@gmail.com>
2025-12-01 03:06:41 +0000

    Second commit

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -1,5 +1,11 @@
 let date = require('moment')().format('YYYYMMDD')
-let commit = require('child_process').execSync('git rev-parse HEAD').toString().slice(0, 5)
+
+// --- OLD CRASHING CODE ---
+// let commit = require('child_process').execSync('git rev-parse HEAD').toString().slice(0, 5)
+
+// --- NEW WORKING CODE ---
+let commit = "00000" // dummy ID
+
 let version = `"${date}-${commit}"`
 
 console.log(`current version is ${version}`)
